b06632158f1dc41c44b0e43d838dbf7e87d73cd3
Sergey Kozel <s.kozel@akveo.com>
2021-01-14 11:44:45 +0300

    first commit

---
+++ android/app/src/main/java/com/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "test";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
